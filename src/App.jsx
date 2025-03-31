11550b3990f3f0ee78481c77e155029cc902cb81
vargasgabrielpio <vargasgabrielpio@gmail.com>
2025-03-31 21:40:35 +0000

    fim da ciracao e estilizacao do segundo card

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import {useState} from "react"
+import illustrationThankYou from "/images/illustration-thank-you.svg"
 import iconStar from "/images/icon-star.svg"
 
 export function App() {
@@ -46,7 +47,15 @@ export function App() {
           <button className="bg-orange w-full py-3 uppercase text-sm rounded-3xl tracking-1 font-bold" onClick={handleSubmit}>Submit</button>
         </div>
       ) : (
-        <p>asrer</p>
+        <div className="bg-gradient-dark text-white mx-6 p-6 rounded-2xl font-overpass">
+          <img className="mx-auto mb-6" src="{illustrationThankYou}" alt="illustration Thank You" />
+
+          <p className="text-orange bg-dark-blue w-fit mx-auto px-3 py-1.25 rounded-3xl mb-6">You selcted {notaAvaliacao} out of 5</p>
+
+          <h1 className="text-2xl font-bold mb-2.5 text-center">Thank you!</h1>
+          <p className="text-light-grey text-sm leading-1 text-center">We appreciate you taking the time to give a rating. If you ever need more support, don't hesitate to get in touch!</p>
+
+        </div>
       )
   )
 }
